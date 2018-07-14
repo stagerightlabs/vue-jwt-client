d9179c528aa6ee86c7632503dee9d15f04f0048d
Ryan Durham <ryan@stagerightlabs.com>
2018-07-14 14:42:42 -0700

    Scaffolding Views and Routes

--- src/router.js
+++ src/router.js
@@ -1,21 +1,66 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import About from './views/About.vue'
+import Login from '@/views/Login.vue'
+import Secrets from '@/views/Secrets.vue'
+import NotFound from '@/views/NotFound.vue'
+import Register from '@/views/Register.vue'
+import Dashboard from '@/views/Dashboard.vue'
+import ResetPassword from '@/views/ResetPassword.vue'
+import ForgotPassword from '@/views/ForgotPassword.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '/register',
+      name: 'Register',
+      component: Register
     },
     {
-      path: '/about',
-      name: 'about',
-      component: About
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/forgot-password',
+      name: 'ForgotPassword',
+      component: ForgotPassword
+    },
+    {
+      path: '/password/reset/:code',
+      name: 'ResetPassword',
+      component: ResetPassword,
+      props: true
+    },
+    {
+      path: '/',
+      name: 'Dashboard',
+      component: Dashboard,
+      beforeEnter: (to, from, next) => {
+        if (window.authority.authenticated()) {
+          return next()
+        }
+
+        return next({ name: 'Login' })
+      }
+    },
+    {
+      path: '/secrets',
+      name: 'Secrets',
+      component: Secrets,
+      beforeEnter: (to, from, next) => {
+        // if (window.authority.authenticated()) {
+        //   return next()
+        // }
+
+        return next({ name: 'Login' })
+      }
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: NotFound
     }
   ]
 })
