e6f1eef014d2a3d8a0ee4d158ca68ab3593e6534
Ryan Durham <ryan@stagerightlabs.com>
2018-07-15 12:34:11 -0700

    Register icon component

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Icon from 'vue-awesome/components/Icon.vue'
 
 // Enable production tips from Vue. (not required)
 // https://vuejs.org/v2/api/#productionTip
 Vue.config.productionTip = true
+
+// Register third-party plugins
+Vue.component('icon', Icon)
 
 // Attempt to read credentials from local storage
 store.dispatch('tryAutoLogin')
