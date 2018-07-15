65606914e9e57fc374d294d7ff5d3aad08ccb7f7
Ryan Durham <ryan@stagerightlabs.com>
2018-07-15 12:34:35 -0700

    Change default test alert content.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,7 +3,14 @@
     <h1>Dashboard</h1>
     <p class="text-center">Hello, {{ email }}.</p>
     <p class="text-center">You are logged in.</p>
-    <p class="w-full text-center my-8"><a class="bg-grey-dark hover:bg-grey-darker text-white font-bold py-2 px-4 m-2 rounded cursor-pointer" @click.prevent="alert('lorem nd prints it out. Each time you refresh  the  page,  the  number  picks  up where it left off. If you open this script on  two  different  computers', 'success')">Test Alert</a></p>
+    <p class="w-full text-center my-8">
+      <a
+        class="bg-grey-dark hover:bg-grey-darker text-white font-bold py-2 px-4 m-2 rounded cursor-pointer"
+        @click.prevent="alert('This is a test message.', 'success')"
+      >
+        Test Alert
+      </a>
+    </p>
   </div>
 </template>
 
